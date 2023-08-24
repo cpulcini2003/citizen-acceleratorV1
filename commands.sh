ec8bb1f89ca2a1f3ab7d72153e0761eff4e10383
Carlo Pulcini <cpulcini2003@gmail.com>
2023-08-25 00:52:16 +0200

    vai

--- commands.sh
+++ commands.sh
@@ -1,3 +1,11 @@
-tanzu accelerator generate-from-local --accelerator-path citizen-accelerator="$(pwd)" \
---server-url https://tap-gui.tap-regular-buck.tapsandbox.com/ \
---options '{"firstConfigurableParameter": "Parameter 1", "usePostgresCheckbox": true, " postgresInfo":"DB-percorso"}' -o "./citizen-accelerator/"
+
+tanzu accelerator generate-from-local --accelerator-path citizen-accelerator="$(pwd)" --server-url  https://tap-gui.tap-powerful-sloth.tapsandbox.com --options '{"usePostgresCheckbox": true, "postgresUrl":"/citizens", "postgresUsername":"postgres", "postgresPassword":"postgres"}' -o "./citizen-accelerator/"
+
+tanzu accelerator create citizen-accelerator --git-repository https://github.com/cpulcini2003/citizen-acceleratorV1 --git-branch main
+
+tanzu service class-claim create customer-database --class postgresql-unmanaged -n cpu
+
+tanzu services class-claims get customer-database --namespace cpu
+
+Claim Reference: 
+  services.apps.tanzu.vmware.com/v1alpha1:ClassClaim:customer-database
